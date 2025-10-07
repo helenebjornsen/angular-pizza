5aded15c9797b4199cef901d4ca6efc78165d961
helenebjornsen <helene.bjornsen@gmail.com>
2025-10-07 09:33:05 +0200

    Added a button switching between memes

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
+
 export class App {
   protected readonly title = signal('Pizza til grads!');
-  protected imagePath = 'assets/img/pizza-meme.jpg'; 
+  //protected imagePath = 'assets/img/pizza-meme.jpg';
+
+  //Array of possible meme paths
+  private readonly memePaths = [
+    'assets/img/pizza-meme.jpg',
+    'assets/img/pizza-meme2.jpg',
+    'assets/img/pizza-meme3.jpg',
+  ];
+
+  //Signal to hold the current image path
+  protected currentMemePath = signal(this.memePaths[0]);
+
+  //Method to change the image when the button is clicked
+  generateNewMeme() {
+    let newIndex: number;
+    let currentPath = this.currentMemePath();
+
+    //Ensure the new image is different from the current one
+    do {
+      newIndex = Math.floor(Math.random() * this.memePaths.length);
+    } while (this.memePaths[newIndex] === currentPath);
+
+    this.currentMemePath.set(this.memePaths[newIndex]);
+
 }
+
+}
+
